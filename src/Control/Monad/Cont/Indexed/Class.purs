f354a83b70383af82f39a33200d1506654bf1566
Gabriela Seabra <gabriela.lima.se@gmail.com>
2020-09-07 17:11:31 -0300

    class

---
+++ src/Control/Monad/Cont/Indexed/Class.purs
@@ -0,0 +1,8 @@
+module Control.Monad.Cont.Indexed where
+
+import Control.Monad.Indexed (class IxMonad)
+
+class IxMonad m <= IxMonadCont m where
+  reset :: forall r o a . m a o o -> m r r a
+  shift :: forall r o j a . ((forall i. a -> m i i o) -> m r j j) -> m r o a
+
